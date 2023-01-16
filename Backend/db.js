0959688240b633a245a6e65d90b6ecb8acfe7df6
sakshibjadhav <79741455+sakshibjadhav@users.noreply.github.com>
2023-01-17 00:16:49 +0530

    worked on authentication api

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/"
+const mongoURI = "mongodb://localhost:27017/save-notes"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,13 +1,50 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+var jwt = require('jsonwebtoken');
+const JWT_SCERET = "Sakshisakshisakshi";
 
+router.post('/createuser', [
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body);
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with email alreay exists" })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secPass,
+        });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SCERET);
+        console.log(authToken);
+        // res.json(user)
+        res.json({ authToken })
+    } catch (error) {
+        console.error(error.message)
+    }
 })
 
 module.exports = router
