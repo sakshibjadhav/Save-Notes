aa7f1d71affb9ed3b32945eb0d3d4dc2e06a0ec5
sakshibjadhav <79741455+sakshibjadhav@users.noreply.github.com>
2023-01-17 23:52:29 +0530

    login added

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -47,4 +47,38 @@ router.post('/createuser', [
     }
 })
 
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Please try to login with correct email" })
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Please try to login with correct password" })
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SCERET);
+        res.json({ authToken })
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error occured");
+    }
+})
 module.exports = router
